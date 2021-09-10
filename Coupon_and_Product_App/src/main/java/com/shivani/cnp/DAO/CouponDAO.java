69fb66a8e6866489c914cddc771800bf751c3ef4
shivanip1512 <shivanipacharne10@gmail.com>
2021-09-10 22:49:29 +0530

    implemented DAO

--- Coupon_and_Product_App/src/main/java/com/shivani/cnp/DAO/CouponDAO.java
+++ Coupon_and_Product_App/src/main/java/com/shivani/cnp/DAO/CouponDAO.java
@@ -1,5 +1,51 @@
 package com.shivani.cnp.DAO;
 
-public class CouponDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.shivani.cnp.Model.Coupon;
+import com.shivani.cnp.Util.ConnectionUtil;
+
+public class CouponDAO {
+	public void save(Coupon coupon) {
+		Connection connection = ConnectionUtil.getConnection();
+		try {
+			PreparedStatement statement = connection
+					.prepareStatement("insert into coupon (code, discount, exp_date) values (?,?,?)");
+			statement.setString(1, coupon.getCode());
+			statement.setBigDecimal(2, coupon.getDiscount());
+			statement.setString(3, coupon.getExpDate());
+
+			int update = statement.executeUpdate();
+
+			if (update > 0) {
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Coupon findByCouponCode(String code) {
+		Coupon coupon = new Coupon();
+		Connection connection = ConnectionUtil.getConnection();
+		try {
+			PreparedStatement statement = connection.prepareStatement("select * from coupon where code=?");
+			statement.setString(1, code);
+
+			ResultSet rs = statement.executeQuery();
+
+			while (rs.next()) {
+				coupon.setId(rs.getInt(1));
+				coupon.setCode(code);
+				coupon.setDiscount(rs.getBigDecimal(3));
+				coupon.setExpDate(rs.getString(4));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return coupon;
+	}
 }
--- Coupon_and_Product_App/src/main/java/com/shivani/cnp/DAO/ProductDAO.java
+++ Coupon_and_Product_App/src/main/java/com/shivani/cnp/DAO/ProductDAO.java
@@ -1,5 +1,24 @@
 package com.shivani.cnp.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.shivani.cnp.Model.Product;
+import com.shivani.cnp.Util.ConnectionUtil;
+
 public class ProductDAO {
+	public void save(Product product) {
+		Connection connection = ConnectionUtil.getConnection();
+		try {
+			PreparedStatement prepareStatement = connection.prepareStatement("insert into product (name, description, price) values (?,?,?)");
+			prepareStatement.setString(1, product.getName());
+			prepareStatement.setString(2, product.getDescription());
+			prepareStatement.setBigDecimal(3, product.getPrice());
+			int update = prepareStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
